0aab2c4549620a0b95f4ab2bee71736e3a67914f
tatekennn <tatekennn@users.noreply.github.com>
2026-06-20 22:23:04 +0900

    fix: use Tokyo timezone for work timestamps

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module JibunOs
     config.autoload_lib(ignore: %w[assets tasks])
 
     config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :utc
     config.autoload_paths << Rails.root.join("app/services")
     config.eager_load_paths << Rails.root.join("app/services")
 
--- test/models/work_day_test.rb
+++ test/models/work_day_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class WorkDayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "application uses Tokyo time zone for work day timestamps" do
+    assert_equal "Tokyo", Rails.application.config.time_zone
+    assert_equal "Tokyo", Time.zone.name
+  end
+
+  test "confirm check in stores current time in Tokyo zone" do
+    travel_to Time.zone.local(2026, 6, 20, 22, 21, 0) do
+      work_day = WorkDay.find_or_create_by!(date: Date.current)
+
+      work_day.confirm_check_in!
+
+      assert_equal "2026-06-20 22:21", I18n.l(work_day.reload.check_in_confirmed_at, format: :long)
+    end
+  end
+
+  test "today follows Tokyo calendar date" do
+    travel_to Time.utc(2026, 6, 20, 15, 30, 0) do
+      assert_equal Date.new(2026, 6, 21), Date.current
+      assert_equal Date.new(2026, 6, 21), WorkDay.today.date
+    end
+  end
 end
